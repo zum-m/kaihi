475afde3c503451420a1a78b17cf3b9e8f4fc941
zum-m <21039m@gmail.com>
2021-10-24 17:09:52 +0900

    [add] registerに趣味タグとルームのインプット欄追加

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -45,6 +45,40 @@
                                 name="password_confirmation" required />
             </div>
 
+            <!-- ルーム名設定 -->
+            <div class="mt-4">
+                <x-label for="room_name" :value="__('roome name')" />
+
+                <x-input id="room_name" class="block mt-1 w-full" type="text" name="room_name" :value="old('room_name')" required autofocus />
+            </div>
+
+
+            <!-- 趣味tagの設定 -->
+            <div class="mt-4">
+                <div>
+                    <input type="text" class="form-control w-50 mb-3" name="new_tag" placeholder="あなたの趣味を入力" />
+                    <button type="submit" class="btn btn-primary">保存</button>
+                </div>
+                <div class="form-check form-check-inline mb-3">
+                    <input class="form-check-input" type="checkbox" name="tags[]" id=" " value=" ">
+                    <label class="form-check-label" for="[]">tagname</label>
+                </div>
+                <div class="form-check form-check-inline mb-3">
+                    <input class="form-check-input" type="checkbox" name="tags[]" id=" " value=" ">
+                    <label class="form-check-label" for="[]">tagname</label>
+                </div>
+                <div class="form-check form-check-inline mb-3">
+                    <input class="form-check-input" type="checkbox" name="tags[]" id=" " value=" ">
+                    <label class="form-check-label" for="[]">tagname</label>
+                </div>
+                <div class="form-check form-check-inline mb-3">
+                    <input class="form-check-input" type="checkbox" name="tags[]" id=" " value=" ">
+                    <label class="form-check-label" for="[]">tagname</label>
+                </div>
+
+            </div>
+
+
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
                     {{ __('Already registered?') }}
@@ -54,6 +88,7 @@
                     {{ __('Register') }}
                 </x-button>
             </div>
+
         </form>
     </x-auth-card>
 </x-guest-layout>
