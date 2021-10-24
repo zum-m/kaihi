dac5f570fe9e0cc098cd3f28f8584f73dd156ea4
zum-m <21039m@gmail.com>
2021-10-24 18:03:54 +0900

    userのマイグレーションファイルの変更

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('room');
+            $table->boolean('room_open')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
